94ba4dc16fe38e043f80623cb95d816dba30496b
Alfar0nt <mamank1243@gmail.com>
2025-09-28 07:43:52 +0700

    add hitungGaji folder

---
+++ hitungGaji/hitungGaji.java
@@ -0,0 +1,30 @@
+package hitungGaji;
+
+import java.util.Scanner;
+
+public class hitungGaji {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+        // input gaji pokok
+        System.out.print("Masukkan gaji pokok: ");
+        double gajiPokok = input.nextDouble();
+
+        // tambah tunjangan
+        double tunjangan = gajiPokok * 0.30;
+
+        // kurangi pajak
+        double potongan = gajiPokok * 0.10;
+
+        // hitung
+        double gajiDiterima = gajiPokok + tunjangan - potongan;
+
+        System.out.println("\n=== Rincian Gaji ===");
+        System.out.println("Gaji Pokok   : Rp " + gajiPokok);
+        System.out.println("Potongan (10%): Rp " + potongan);
+        System.out.println("Tunjangan (30%): Rp " + tunjangan);
+        System.out.println("Gaji Diterima : Rp " + gajiDiterima);
+
+        input.close();
+    }
+}
---
+++ tiketKereta/readme.md
@@ -0,0 +1,10 @@
+# Aplikasi Tiket Kereta
+Berdasarkan Jenis Tiket user, jumlah tiket yang dibeli
+Tugas BP 1 pertemuan 5
+![Foto tugas](tugasTiketKereta.png)
+
+## Deskripsi
+aaa
+
+## Flowchart
+menyusul
