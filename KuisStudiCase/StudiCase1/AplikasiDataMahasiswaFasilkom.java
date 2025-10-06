ca8f39862626ca4a86737038aa823c8e71908e66
Alfar0nt <mamank1243@gmail.com>
2025-10-06 09:22:06 +0700

    Update StudiCase1, readme, foto tugas

--- KuisStudiCase/StudiCase1/AplikasiDataMahasiswaFasilkom.java
+++ KuisStudiCase/StudiCase1/AplikasiDataMahasiswaFasilkom.java
@@ -1,4 +1,4 @@
-package UlanganTengahSemester.StudiCase1;
+package KuisStudiCase.StudiCase1;
 import java.util.Scanner;
 
 public class AplikasiDataMahasiswaFasilkom {
@@ -20,29 +20,42 @@ public class AplikasiDataMahasiswaFasilkom {
 
             // Percabangan dengan perulangan for di dalamnya
             if (pilihan == 1) {
-                for (int i = 0; i < 1; i++) { // perulangan for hanya sekali
+                System.out.print("Masukkan jumlah mahasiswa Sistem Informasi yang hadir: ");
+                int jumlah = input.nextInt();
+                for (int i = 0; i < jumlah; i++) {
                     sistemInformasi++;
-                    System.out.println("Data kehadiran untuk Sistem Informasi berhasil ditambahkan!");
                 }
-            } else if (pilihan == 2) {
-                for (int i = 0; i < 1; i++) {
+                System.out.println("Data kehadiran untuk Sistem Informasi sebanyak " + jumlah + " berhasil ditambahkan!");
+            } 
+            else if (pilihan == 2) {
+                System.out.print("Masukkan jumlah mahasiswa Informatika yang hadir: ");
+                int jumlah = input.nextInt();
+                for (int i = 0; i < jumlah; i++) {
                     informatika++;
-                    System.out.println("Data kehadiran untuk Informatika berhasil ditambahkan!");
                 }
-            } else if (pilihan == 3) {
-                for (int i = 0; i < 1; i++) {
+                System.out.println("Data kehadiran untuk Informatika sebanyak " + jumlah + " berhasil ditambahkan!");
+            } 
+            else if (pilihan == 3) {
+                System.out.print("Masukkan jumlah mahasiswa Sains Data yang hadir: ");
+                int jumlah = input.nextInt();
+                for (int i = 0; i < jumlah; i++) {
                     sainsData++;
-                    System.out.println("Data kehadiran untuk Sains Data berhasil ditambahkan!");
                 }
-            } else {
+                System.out.println("Data kehadiran untuk Sains Data sebanyak " + jumlah + " berhasil ditambahkan!");
+            } 
+            else {
                 System.out.println("Pilihan tidak valid!");
             }
-
+               System.out.println("\n=== Rekap Kehadiran Mahasiswa ===");
+                System.out.println("Sistem Informasi : " + sistemInformasi + " mahasiswa");
+                System.out.println("Informatika      : " + informatika + " mahasiswa");
+                System.out.println("Sains Data       : " + sainsData + " mahasiswa");
             System.out.print("Apakah ingin menginputkan data lagi? (1 = ya) (2 = tidak): ");
             ulang = input.nextInt();
 
         } while (ulang == 1);
 
+        // Menampilkan hasil akhir menggunakan switch
         switch (ulang) {
             case 2:
                 System.out.println("\n=== Rekap Kehadiran Mahasiswa ===");
