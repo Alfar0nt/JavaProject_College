cb43ebcbd815e57446e0ff4977879129aeed9fcb
Alfar0nt <mamank1243@gmail.com>
2025-10-06 08:25:32 +0700

    add studicase folder

---
+++ KuisStudiCase/StudiCase1/AplikasiDataMahasiswaFasilkom.java
@@ -0,0 +1,61 @@
+package UlanganTengahSemester.StudiCase1;
+import java.util.Scanner;
+
+public class AplikasiDataMahasiswaFasilkom {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int sistemInformasi = 0;
+        int informatika = 0;
+        int sainsData = 0;
+        int ulang;
+
+        do {
+            System.out.println("=== Input Kehadiran Mahasiswa Fasilkom ===");
+            System.out.println("1. Sistem Informasi");
+            System.out.println("2. Informatika");
+            System.out.println("3. Sains Data");
+            System.out.print("Pilih prodi (1-3): ");
+            int pilihan = input.nextInt();
+
+            // Percabangan dengan perulangan for di dalamnya
+            if (pilihan == 1) {
+                for (int i = 0; i < 1; i++) { // perulangan for hanya sekali
+                    sistemInformasi++;
+                    System.out.println("Data kehadiran untuk Sistem Informasi berhasil ditambahkan!");
+                }
+            } else if (pilihan == 2) {
+                for (int i = 0; i < 1; i++) {
+                    informatika++;
+                    System.out.println("Data kehadiran untuk Informatika berhasil ditambahkan!");
+                }
+            } else if (pilihan == 3) {
+                for (int i = 0; i < 1; i++) {
+                    sainsData++;
+                    System.out.println("Data kehadiran untuk Sains Data berhasil ditambahkan!");
+                }
+            } else {
+                System.out.println("Pilihan tidak valid!");
+            }
+
+            System.out.print("Apakah ingin menginputkan data lagi? (1 = ya) (2 = tidak): ");
+            ulang = input.nextInt();
+
+        } while (ulang == 1);
+
+        switch (ulang) {
+            case 2:
+                System.out.println("\n=== Rekap Kehadiran Mahasiswa ===");
+                System.out.println("Sistem Informasi : " + sistemInformasi + " mahasiswa");
+                System.out.println("Informatika      : " + informatika + " mahasiswa");
+                System.out.println("Sains Data       : " + sainsData + " mahasiswa");
+                break;
+        
+            default:
+                System.out.println("Input " + ulang + " tidak ditemukan");
+                break;
+        }
+
+        input.close();
+    }
+}
