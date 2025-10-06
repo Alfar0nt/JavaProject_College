701fca14f9e7b5e6eff2e50f9507af4740097ecc
Alfar0nt <mamank1243@gmail.com>
2025-10-06 07:39:23 +0700

    update avgCalculation and smallestLargest to be more simple

--- loopExercise/averageCalculation.java
+++ loopExercise/averageCalculation.java
@@ -2,59 +2,48 @@ package loopExercise;
 import java.util.Scanner;
 
 public class averageCalculation {
-     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n;
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
         double number, total, average;
 
         // === Using FOR loop ===
         System.out.println("=== Using FOR loop ===");
-        System.out.print("Enter the number of numbers: ");
-        n = input.nextInt();
-
         total = 0;
-        for (int i = 1; i <= n; i++) {
-            System.out.print("Enter the " + i + " number: ");
+        for (int i = 1; i <= 3; i++) {
+            System.out.print("Enter the " + i + " number = ");
             number = input.nextDouble();
             total += number;
         }
-        average = total / n;
-        System.out.println("The average value of " + n + " numbers is " + average);
+        average = total / 3;
+        System.out.println("The average value of 3 numbers is " + average);
         System.out.println();
 
         // === Using WHILE loop ===
         System.out.println("=== Using WHILE loop ===");
-        System.out.print("Enter the number of numbers: ");
-        n = input.nextInt();
-
         int i = 1;
         total = 0;
-        while (i <= n) {
-            System.out.print("Enter the " + i + " number: ");
+        while (i <= 3) {
+            System.out.print("Enter the " + i + " number = ");
             number = input.nextDouble();
             total += number;
             i++;
         }
-        average = total / n;
-        System.out.println("The average value of " + n + " numbers is " + average);
+        average = total / 3;
+        System.out.println("The average value of 3 numbers is " + average);
         System.out.println();
 
         // === Using DO-WHILE loop ===
         System.out.println("=== Using DO-WHILE loop ===");
-        System.out.print("Enter the number of numbers: ");
-        n = input.nextInt();
-
         i = 1;
         total = 0;
         do {
-            System.out.print("Enter the " + i + " number: ");
+            System.out.print("Enter the " + i + " number = ");
             number = input.nextDouble();
             total += number;
             i++;
-        } while (i <= n);
-
-        average = total / n;
-        System.out.println("The average value of " + n + " numbers is " + average);
+        } while (i <= 3);
+        average = total / 3;
+        System.out.println("The average value of 3 numbers is " + average);
 
         input.close();
     }
--- loopExercise/smallestLargestNumberCalculation.java
+++ loopExercise/smallestLargestNumberCalculation.java
@@ -4,73 +4,55 @@ import java.util.Scanner;
 public class smallestLargestNumberCalculation {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int n;
         double number, smallest, largest;
+        int i;
 
-        // === Using FOR loop ===
+        // === FOR loop ===
         System.out.println("=== Using FOR loop ===");
-        System.out.print("Enter number of data: ");
-        n = input.nextInt();
-
-        System.out.print("Enter the 1st number: ");
+        System.out.print("Enter the 1st number = ");
         number = input.nextDouble();
-        smallest = largest = number; // initial value
+        smallest = largest = number;
 
-        for (int i = 2; i <= n; i++) {
-            System.out.print("Enter the " + i + " number: ");
+        for (i = 2; i <= 3; i++) {
+            System.out.print("Enter the " + i + " number = ");
             number = input.nextDouble();
             if (number < smallest) smallest = number;
             if (number > largest) largest = number;
         }
-
         System.out.println("Smallest Number = " + smallest);
         System.out.println("Largest Number = " + largest);
         System.out.println();
 
-        // === Using WHILE loop ===
+        // === WHILE loop ===
         System.out.println("=== Using WHILE loop ===");
-        System.out.print("Enter number of data: ");
-        n = input.nextInt();
-
-        int i = 1;
-        System.out.print("Enter the 1st number: ");
+        System.out.print("Enter the 1st number = ");
         number = input.nextDouble();
         smallest = largest = number;
-
-        i++;
-        while (i <= n) {
-            System.out.print("Enter the " + i + " number: ");
+        i = 2;
+        while (i <= 3) {
+            System.out.print("Enter the " + i + " number = ");
             number = input.nextDouble();
             if (number < smallest) smallest = number;
             if (number > largest) largest = number;
             i++;
         }
-
         System.out.println("Smallest Number = " + smallest);
         System.out.println("Largest Number = " + largest);
         System.out.println();
 
-        // === Using DO-WHILE loop ===
+        // === DO-WHILE loop ===
         System.out.println("=== Using DO-WHILE loop ===");
-        System.out.print("Enter number of data: ");
-        n = input.nextInt();
-
-        i = 1;
-        System.out.print("Enter the 1st number: ");
+        System.out.print("Enter the 1st number = ");
         number = input.nextDouble();
         smallest = largest = number;
-
-        i++;
+        i = 2;
         do {
-            if (i <= n) {
-                System.out.print("Enter the " + i + " number: ");
-                number = input.nextDouble();
-                if (number < smallest) smallest = number;
-                if (number > largest) largest = number;
-                i++;
-            }
-        } while (i <= n);
-
+            System.out.print("Enter the " + i + " number = ");
+            number = input.nextDouble();
+            if (number < smallest) smallest = number;
+            if (number > largest) largest = number;
+            i++;
+        } while (i <= 3);
         System.out.println("Smallest Number = " + smallest);
         System.out.println("Largest Number = " + largest);
 
