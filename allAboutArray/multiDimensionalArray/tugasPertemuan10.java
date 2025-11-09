da315039dcc4f7cb189e2466489691790b37194c
Alfar0nt <mamank1243@gmail.com>
2025-11-09 22:29:41 +0700

    revisi tugasPert10

--- allAboutArray/multiDimensionalArray/tugasPertemuan10.java
+++ allAboutArray/multiDimensionalArray/tugasPertemuan10.java
@@ -1,18 +1,10 @@
 package allAboutArray.multiDimensionalArray;
 
 public class tugasPertemuan10 {
- public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Assignment - Create Random Image object using Array");
         System.out.println("Random Emoji Generator\n");
         
-        // Generate 1 random emoji
-        System.out.println("Your Random Emoji:");
-        char[][] emoji = generateRandomEmoji();
-        printEmoji(emoji);
-    }
-    
-    // Method to generate a random emoji
-    public static char[][] generateRandomEmoji() {
         // Define the 4 specified emoji patterns
         char[][][] emojiTemplates = {
             // First emoji: <(￣︶￣)>
@@ -22,8 +14,8 @@ public class tugasPertemuan10 {
             },
             // Second emoji: ＼(￣▽￣)／
             {
-                {' ', ' ', '￣', ' ', ' ','￣', ' '},
-                {'＼', '(', ' ', '▽', ' ', ')', '／'}
+                {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {'＼', '(', '￣', '▽', '￣', ')', '／'}
             },
             // Third emoji: (≧◡≦)
             {
@@ -39,14 +31,13 @@ public class tugasPertemuan10 {
         
         // Randomly select one emoji template
         int randomIndex = (int)(Math.random() * emojiTemplates.length);
-        return emojiTemplates[randomIndex];
-    }
-    
-    // Method to print the emoji
-    public static void printEmoji(char[][] emoji) {
-        for (int i = 0; i < emoji.length; i++) {
-            for (int j = 0; j < emoji[i].length; j++) {
-                System.out.print(emoji[i][j]);
+        char[][] selectedEmoji = emojiTemplates[randomIndex];
+        
+        // Print the selected emoji
+        System.out.println("Your Random Emoji:");
+        for (int i = 0; i < selectedEmoji.length; i++) {
+            for (int j = 0; j < selectedEmoji[i].length; j++) {
+                System.out.print(selectedEmoji[i][j]);
             }
             System.out.println();
         }
